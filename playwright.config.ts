e078984f28dca5e672551ffc2fc3fc15a1cd04fc
Abilash Chintalacheruvula <abilashchintalacheruvula@Abilashs-iMac.local>
2026-02-18 21:29:28 -0500

    Adding github actions yaml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './src/test/web',
+  // testDir: './src/test/api',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
